a8c5dfb4961a8755b65756173723d1172d390e28
maninder1046 <90817762+maninder1046@users.noreply.github.com>
2022-03-29 14:19:17 +0530

    Update q1.cpp

--- labQ/q1.cpp
+++ labQ/q1.cpp
@@ -9,7 +9,7 @@ void InsertionSort(int a[],int n)
     for(i=1;i<n;i++){
         key=a[i];
         for(j=i-1;j>=0;j--){
-            compare++;
+            compare++;         
             if(key < a[j]){
               a[j+1]=a[j];
               write++;
